de4edf1f13121483355692353984f57475fc7753
Dee Anzorge <d.anzorge@gmail.com>
2025-08-11 22:27:42 +0200

    nix: update for `services.postgresql.settings.port` option name change

--- nix/module.nix
+++ nix/module.nix
@@ -178,7 +178,7 @@ in
 
       port = mkOption {
         type = types.int;
-        default = config.services.postgresql.port;
+        default = config.services.postgresql.settings.port;
         description = "Port of the Postgresql server to connect to.";
       };
 
